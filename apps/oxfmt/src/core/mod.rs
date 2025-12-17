f5981aac56a676482d1958e31eaa3f2cd066eee2
Yuji Sugiura <y.sugiura.0316@gmail.com>
2025-12-17 16:42:11 +0900

    refactor(oxfmt): Prepare for .editorconfig support

--- apps/oxfmt/src/core/mod.rs
+++ apps/oxfmt/src/core/mod.rs
@@ -6,7 +6,7 @@ pub mod utils;
 #[cfg(feature = "napi")]
 mod external_formatter;
 
-pub use config::{load_config, resolve_config_path};
+pub use config::{ConfigResolver, ResolvedOptions, resolve_config_path};
 pub use format::{FormatResult, SourceFormatter};
 pub use support::FormatFileStrategy;
 
